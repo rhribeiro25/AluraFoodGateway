20ad3060a44e6339a19836c11478723abd63402c
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-08-30 02:50:57 +0100

    adjusts in gateway configuration

--- src/main/java/br/com/alurafood/gateway/GatewayApplication.java
+++ src/main/java/br/com/alurafood/gateway/GatewayApplication.java
@@ -1,12 +1,8 @@
 package br.com.alurafood.gateway;
 
-import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import reactor.netty.http.client.HttpClient;
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
